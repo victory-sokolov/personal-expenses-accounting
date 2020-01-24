22ec7542a1dfa998e6ea3df9089ce23b3211b9ee
victory-sokolov <viktorsokolov.and@gmail.com>
2020-01-24 18:02:57 +0200

    modified generate training script

--- app/tesseract/generate_training_data.sh
+++ app/tesseract/generate_training_data.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-rm -rf train/*
-tesstrain.sh --fonts_dir fonts \
-            --fontlist 'Agency FB Condensed' \
-            --lang eng \
+START=$(date +%s)
+
+rm -rf train_data/*
+tesstrain.sh --fonts_dir ../../fonts/ \
+            --fontlist 'HypermarketW00-Light Light' \
+            --lang lav \
             --linedata_only \
-            --langdata_dir $TESSERACT/langdata_lstm \
+            --langdata_dir $TESSDATA_PREFIX/langdata_lstm \
             --tessdata_dir $TESSDATA_PREFIX \
             --save_box_tiff \
-            --maxpages 10 \
-            --output_dir train
+            --maxpages 300 \
+            --output_dir train_data
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+
+echo "Execution time: $DIFF seconds"
