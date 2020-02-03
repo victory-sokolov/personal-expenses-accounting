6d3e94a34a673358160cb1cd6a0a27c19b114f91
victory-sokolov <viktorsokolov.and@gmail.com>
2020-02-03 21:42:21 +0200

    updated font.py script

--- app/utils/font.py
+++ app/utils/font.py
@@ -1,31 +1,32 @@
-from subprocess import Popen, PIPE, check_output
-from pathlib import Path, PurePosixPath
 import os
 import re
+from pathlib import Path, PurePosixPath
+from subprocess import PIPE, Popen, check_output
 
 
-def path() -> str:
+def font_path() -> str:
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     path = PurePosixPath(ROOT_DIR).parent.parent
     return path
 
 
 def get_fonts_names() -> list:
-    dir = f'{path()}/fonts/'
+    dir = f'{font_path()}/fonts/'
     return os.listdir(dir)
-  
+
 
 def font_name(fonts: list) -> list:
-    font_lst = [] 
+    font_lst = []
     font_names = fonts()
 
     for name in font_names:
-        process = Popen(['fc-scan', f'{path()}/fonts/{name}'], stdout=PIPE, stderr=PIPE)
+        process = Popen(
+            ['fc-scan', f'{font_path()}/fonts/{name}'], stdout=PIPE, stderr=PIPE)
         full_name = check_output(('grep', 'fullname'), stdin=process.stdout)
         process.wait()
         font_name = re.findall(r'\"(.+?)\"', str(full_name).split(":")[1])[0]
         font_lst.append(font_name)
-    return font_lst    
+    return font_lst
 
 
-print(font_name(get_fonts_names))
+# print(font_name(get_fonts_names))
