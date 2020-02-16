fa447dd72ec5b548de338967ecd6fe356f9ddd5f
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-16 20:54:41 +0200

    fixed bugs

--- app/tesseract/tesseract.py
+++ app/tesseract/tesseract.py
@@ -7,35 +7,35 @@ from app.utils.font import font_path, get_fonts_names
 
 LANG = "lav"
 TRAIN_FOLDER = "train_data"
-MODEL = "model_output/"
+MODEL = "model_output"
 TESSERACT_ENV = os.getenv("TESSDATA_PREFIX")
 
 
 def generate_training_data():
-    max_pages = 20
+    max_pages = 250
     path = font_path()
     fonts = get_fonts_names()
 
-    process = subprocess.call([
+    process = subprocess.check_output([
         'tesstrain.sh',
-        f'--fonts_dir {path}',
-        f'--fontlist {fonts}',
-        f'--lang {LANG}',
+        '--fonts_dir', path,
+        '--fontlist', 'HypermarketW00-Light Light',
+        '--lang', LANG,
         '--noextract_font_properties',
         '--linedata_only',
-        f'--langdata_dir {TESSERACT_ENV}/langdata_lstm',
-        f'--tessdata_dir {TESSERACT_ENV}',
+        '--langdata_dir', f'{TESSERACT_ENV}/langdata_lstm',
+        '--tessdata_dir', TESSERACT_ENV,
         '--save_box_tiff',
-        f'--maxpages {max_pages}',
-        f'--output_dir {TRAIN_FOLDER}'
-    ])
-    process.wait()
+        '--maxpages', str(max_pages),
+        '--output_dir', TRAIN_FOLDER
+    ], text=True)
+
     print(process)
 
 
 def evaluate():
     font_checkpoint = "hypermarket"
-    logfile = open('logfile', 'w')
+    #logfile = open('logfile', 'w')
 
     process = subprocess.check_output([
         'lstmeval',
@@ -53,14 +53,15 @@ def evaluate_default():
         '--model', f'{LANG}.lstm',
         '--traineddata', f'{TESSERACT_ENV}/{LANG}.traineddata',
         '--eval_listfile', f'{TRAIN_FOLDER}/{LANG}.training_files.txt'
-    ])
+    ], text=True)
+
     print(process)
 
 
 def combine():
     checkpoint = "hypermarket"
 
-    process = subprocess.check_output([
+    process = subprocess.run([
         'lstmtraining',
         '--stop_training',
         '--continue_from', f'model_output/{checkpoint}_checkpoint',
@@ -84,10 +85,13 @@ def fine_tune():
         '--model_output', f'{MODEL}/hypermarket',
         '--traineddata', f'{TESSERACT_ENV}/{LANG}.traineddata',
         '--train_listfile', f'{TRAIN_FOLDER}/{LANG}.training_files.txt',
-        '--max_iterations', '400'
-    ])
+        '--max_iterations', '2000'
+    ], text=True)
     print(process)
 
 
-extract_recognition_model()
+# generate_training_data()
+# extract_recognition_model()
 # fine_tune()
+# combine()
+evaluate()
