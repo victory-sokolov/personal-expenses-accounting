f9f83f8f504caa1dc6906d935d076fc197bbeb81
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-16 17:39:45 +0200

    refactored

--- app/utils/font.py
+++ app/utils/font.py
@@ -7,26 +7,22 @@ from subprocess import PIPE, Popen, check_output
 def font_path() -> str:
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     path = PurePosixPath(ROOT_DIR).parent.parent
-    return path
+    return f'{path}/fonts'
 
 
 def get_fonts_names() -> list:
-    dir = f'{font_path()}/fonts/'
+    dir = f'{font_path()}'
     return os.listdir(dir)
 
 
 def font_name(fonts: list) -> list:
     font_lst = []
-    font_names = fonts()
 
-    for name in font_names:
+    for name in fonts:
         process = Popen(
-            ['fc-scan', f'{font_path()}/fonts/{name}'], stdout=PIPE, stderr=PIPE)
+            ['fc-scan', f'{font_path()}/{name}'], stdout=PIPE, stderr=PIPE)
         full_name = check_output(('grep', 'fullname'), stdin=process.stdout)
         process.wait()
         font_name = re.findall(r'\"(.+?)\"', str(full_name).split(":")[1])[0]
         font_lst.append(font_name)
     return font_lst
-
-
-# print(font_name(get_fonts_names))
