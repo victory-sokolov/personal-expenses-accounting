568125729a216e962ff572f9a11f6020be9acad6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-18 21:19:58 +0200

    refactored code

--- app/tesseract/training.py
+++ app/tesseract/training.py
@@ -1,9 +1,12 @@
 import os
+import re
 import subprocess
 import sys
 from subprocess import PIPE, Popen, check_output
 
-from app.utils.font import font_path, get_fonts_names
+from app.utils.decor import exectime
+from app.utils.font import font_path
+from app.utils.helpers import read_file
 
 LANG = "lav"
 
@@ -31,47 +34,78 @@ class Training:
             raise MyException('Argument must be list of languages')
         self._langs = langs
 
-    def generate_training_data(self):
-        max_pages = 250
+    def generate_training_data(self, pages=250):
+        """Generates training data"""
         path = font_path()
-        fonts = get_fonts_names()
+        font_list = read_file('fonts.txt')
 
-        process = subprocess.check_output([
+        process = subprocess.call([
             'tesstrain.sh',
             '--fonts_dir', path,
-            '--fontlist', 'HypermarketW00-Light Light',
-            '--lang', LANG,
+            '--fontlist', 'DPix_8pt',
+            '--lang', 'eng',
             '--noextract_font_properties',
             '--linedata_only',
-            '--langdata_dir', f'{TESSERACT_ENV}/langdata_lstm',
-            '--tessdata_dir', TESSERACT_ENV,
+            '--langdata_dir', f'{self.TESSERACT_ENV}/langdata_lstm',
+            '--tessdata_dir', self.TESSERACT_ENV,
             '--save_box_tiff',
-            '--maxpages', str(max_pages),
+            '--maxpages', str(pages),
             '--output_dir', self.TRAIN_FOLDER
-        ], text=True)
+        ])
+        # return process
 
-        print(process)
-
-    def evaluate(self):
-        font_checkpoint = "hypermarket"
-
-        process = subprocess.check_output([
-            'lstmeval',
-            '--model', f'{self.MODEL}/{font_checkpoint}_checkpoint',
-            '--traineddata', f'{self.MODEL}/{LANG}.traineddata',
-            '--eval_listfile', f'{self.TRAIN_FOLDER}/{LANG}.training_files.txt'
+    def extract_recognition_model(self):
+        process = subprocess.call([
+            'combine_tessdata', '-e',
+            f'{self.TESSERACT_ENV}/{LANG}.traineddata',
+            f'{LANG}.lstm'
         ])
 
-        print(process)
+    def evaluate(self, default=False):
+        """
+        Evaluates default Tesseract models for specified languages
 
-    def evaluate_default(self):
-        process = subprocess.check_output([
+        Parameters:
+        arg1(boolean): True if need to evaluate default language model
+
+        Returns:
+        dict: result of evaluation
+        """
+        if default:
+            model = f'{LANG}.lstm'
+            traineddata = f'{self.TESSERACT_ENV}/{LANG}.traineddata'
+        else:
+            font_checkpoint = "hypermarket"
+            model = f'{self.MODEL}/{font_checkpoint}_checkpoint'
+            traineddata = f'{self.MODEL}/{LANG}.traineddata'
+
+        process = subprocess.Popen([
             'lstmeval',
-            '--model', f'{LANG}.lstm',
-            '--traineddata', f'{TESSERACT_ENV}/{LANG}.traineddata',
+            '--model', model,
+            '--traineddata', traineddata,
             '--eval_listfile', f'{self.TRAIN_FOLDER}/{LANG}.training_files.txt'
-        ], text=True)
+        ], stdout=PIPE, stderr=subprocess.STDOUT, text=True)
 
+        out = process.communicate()
+        last_line = out[0].split('\n')[-2].split("=")
+
+        char_error = re.findall("\d+\.\d+", last_line[-2])[0]
+        word_error = last_line[-1]
+
+        return {
+            'character_error': float(char_error),
+            'word_error': float(word_error)
+        }
+
+    def fine_tune(self):
+        process = subprocess.check_output([
+            'lstmtraining',
+            '--continue_from', f'{LANG}.lstm',
+            '--model_output', f'{self.MODEL}/hypermarket',
+            '--traineddata', f'{TESSERACT_ENV}/{LANG}.traineddata',
+            '--train_listfile', f'{self.TRAIN_FOLDER}/{LANG}.training_files.txt',
+            '--max_iterations', '2000'
+        ], text=True)
         print(process)
 
     def combine(self):
@@ -85,33 +119,18 @@ class Training:
             '--model_output', f'{self.MODEL}/{LANG}.traineddata'
         ])
 
-    def extract_recognition_model(self):
-        process = subprocess.check_output([
-            'combine_tessdata', '-e',
-            f'{TESSERACT_ENV}/{LANG}.traineddata',
-            f'{LANG}.lstm'
-        ])
-
-    def fine_tune(self):
-        process = subprocess.check_output([
-            'lstmtraining',
-            '--continue_from', f'{LANG}.lstm',
-            '--model_output', f'{self.MODEL}/hypermarket',
-            '--traineddata', f'{TESSERACT_ENV}/{LANG}.traineddata',
-            '--train_listfile', f'{self.TRAIN_FOLDER}/{LANG}.training_files.txt',
-            '--max_iterations', '2000'
-        ], text=True)
-        print(process)
-
     def training_pipeline(self):
         pass
 
 
-# generate_training_data()
 # extract_recognition_model()
 # fine_tune()
 # combine()
 # evaluate()
 train = Training()
-print(train.langs)
+train.generate_training_data(5)
+# print(train.evaluate(True))
+
+# train.evaluate(True)
+# print(train.langs)
 # print(train.langs)
