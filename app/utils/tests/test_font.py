fabe9c838e6d4453c81b2647cec1bc3f02cf8032
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-20 22:32:56 +0200

    test for func is_lang_supported

--- app/utils/tests/test_font.py
+++ app/utils/tests/test_font.py
@@ -6,9 +6,10 @@ from app.utils.font import is_lang_supported
 class TestFont(unittest.TestCase):
 
     def test_is_lang_supported(self):
-        # self.assertTrue(is_lang_supported('OCR-A', 'eng'))
+        self.assertTrue(is_lang_supported('OCR-A', 'eng'))
+        self.assertTrue(is_lang_supported('Hypermarket W00 Light', 'lav'))
         self.assertFalse(is_lang_supported('OCR-A', 'lav'))
 
 
 if __name__ == "__main__":
-    unittest.main(warnings='ignore')
+    unittest.main(verbosity=2)
