c8e7633434955438c65a4e2eff49de2e40b3c5d3
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-21 11:55:22 +0200

    test for converting to iso_639

--- app/utils/tests/test_font.py
+++ app/utils/tests/test_font.py
@@ -1,6 +1,9 @@
 import unittest
 
-from app.utils.font import is_lang_supported
+from app.utils.font import (
+    is_lang_supported,
+    convert_to_iso_639_1
+)
 
 
 class TestFont(unittest.TestCase):
@@ -10,6 +13,9 @@ class TestFont(unittest.TestCase):
         self.assertTrue(is_lang_supported('Hypermarket W00 Light', 'lav'))
         self.assertFalse(is_lang_supported('OCR-A', 'lav'))
 
+    def test_convert_to_iso_639_1(self):
+        self.assertEqual(convert_to_iso_639_1('lav'), 'lv')
+        self.assertEqual(convert_to_iso_639_1('eng'), 'en')
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
