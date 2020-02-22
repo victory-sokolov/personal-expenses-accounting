02d4e71dd9c1401913e98c66ed3a34a3fb8351aa
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-22 12:27:03 +0200

    pass list to --font-list parameter

--- app/services/tesseract/TrainingDataGenerator.py
+++ app/services/tesseract/TrainingDataGenerator.py
@@ -1,35 +1,41 @@
 import subprocess
 
-from app.utils.font import font_path, is_lang_supported
+from app.services.tesseract.ModelProperties import ModelProperties
+from app.utils.font import font_path, get_fonts_names, supported_fonts
 from app.utils.helpers import read_file
+from app.utils.TaskLoggerDecorator import TaskLoggerDecorator
 
 
-class TrainingDataGenerator:
+class TrainingDataGenerator(object):
 
-    def __init__(self, lang: str):
-        self.lang = lang
+    def __init__(self, lang: str, props: ModelProperties):
+        self._lang = lang
+        self.props = props(self._lang, 'model')
 
     def generate_training_data(self, pages=250):
         """Generates training data"""
         path = font_path()
-        font_list = read_file('fonts.txt')
+        font_list = supported_fonts(get_fonts_names(), self._lang)
+        print(font_list)
 
-        # if language is supported then generate training data
-        font = 'OCR-A'
-        if is_lang_supported(font, self.lang):
-            process = subprocess.call([
-                'tesstrain.sh',
-                '--fonts_dir', path,
-                '--fontlist', font,
-                '--lang', self.lang,
-                '--noextract_font_properties',
-                '--linedata_only',
-                '--langdata_dir', f'{self.tesseract_env}/langdata_lstm',
-                '--tessdata_dir', self.tesseract_env,
-                '--save_box_tiff',
-                '--maxpages', str(pages),
-                '--output_dir', self.train_folder
-            ])
-        else:
-            print(f"Font {font} doesn't support {self.lang} language")
+        process = subprocess.call([
+            'tesstrain.sh',
+            '--fonts_dir', path,
+            '--fontlist', *font_list,
+            '--lang', self._lang,
+            '--noextract_font_properties',
+            '--linedata_only',
+            '--langdata_dir', f'{self.props.tesseract_env}/langdata_lstm',
+            '--tessdata_dir', self.props.tesseract_env,
+            '--save_box_tiff',
+            '--maxpages', str(pages),
+            '--output_dir', self.props.training_data
+        ])
+
         return process
+
+
+train = TrainingDataGenerator('lav', ModelProperties)
+train.generate_training_data(5)
+# log = TaskLoggerDecorator(TrainingDataGenerator('lav', ModelProperties))
+# print(log)
