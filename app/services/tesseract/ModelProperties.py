1c1a49b2bffabb3ea950668586b6228eab216918
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-23 22:12:20 +0200

    added more Model properties

--- app/services/tesseract/ModelProperties.py
+++ app/services/tesseract/ModelProperties.py
@@ -1,12 +1,12 @@
 import os
 
-
 class ModelProperties(object):
 
     model_path = os.getenv('MODEl_PATH')
     tesseract_env = os.getenv("TESSDATA_PREFIX")
     training_data = os.getenv("TRAINING_DATA")
+    iterations = 4500
+    pages = 5  # amount of training data (pages)
 
     def __init__(self, lang):
         self.lang = lang
-        # self.model_name = model_name
