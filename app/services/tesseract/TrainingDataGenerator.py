42cfe7e86838bcf5514db8da608d0a48980e54fd
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-23 22:13:14 +0200

    modified font_list

--- app/services/tesseract/TrainingDataGenerator.py
+++ app/services/tesseract/TrainingDataGenerator.py
@@ -1,22 +1,23 @@
 import subprocess
 
 from app.services.tesseract.ModelProperties import ModelProperties
-from app.utils.font import font_path, get_fonts_names, supported_fonts
+from app.utils.font import (font_path, fonts_names, get_font_names,
+                            get_fonts_names_in_dir, supported_fonts)
 from app.utils.helpers import read_file
-from app.utils.TaskLoggerDecorator import TaskLoggerDecorator
+from app.utils.TaskTimerDecorator import TaskTimerDecorator
 
 
 class TrainingDataGenerator(object):
 
     def __init__(self, lang: str, props: ModelProperties):
         self._lang = lang
-        self.props = props(self._lang, 'model')
+        self._props = props
 
-    def generate_training_data(self, pages=250):
+    def generate_training_data(self):
         """Generates training data"""
         path = font_path()
-        font_list = supported_fonts(get_fonts_names(), self._lang)
-        print(font_list)
+        font_support = supported_fonts(get_fonts_names_in_dir(), self._lang)
+        font_list = get_font_names(font_support)
 
         process = subprocess.call([
             'tesstrain.sh',
@@ -25,17 +26,11 @@ class TrainingDataGenerator(object):
             '--lang', self._lang,
             '--noextract_font_properties',
             '--linedata_only',
-            '--langdata_dir', f'{self.props.tesseract_env}/langdata_lstm',
-            '--tessdata_dir', self.props.tesseract_env,
+            '--langdata_dir', f'{self._props.tesseract_env}/langdata_lstm',
+            '--tessdata_dir', self._props.tesseract_env,
             '--save_box_tiff',
-            '--maxpages', str(pages),
-            '--output_dir', self.props.training_data
+            '--maxpages', str(self._props.pages),
+            '--output_dir', self._props.training_data
         ])
 
         return process
-
-
-train = TrainingDataGenerator('lav', ModelProperties)
-train.generate_training_data(5)
-# log = TaskLoggerDecorator(TrainingDataGenerator('lav', ModelProperties))
-# print(log)
