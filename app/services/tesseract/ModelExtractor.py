92526542945c68b099db4e748c8f4e44cb8ea7f7
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-26 21:00:51 +0200

    ModelExtractor class

---
+++ app/services/tesseract/ModelExtractor.py
@@ -0,0 +1,19 @@
+import subprocess
+from app.services.tesseract.ProcessManager import ProcessManager
+from app.services.tesseract.ModelProperties import ModelProperties
+
+class ModelExtractor(object):
+
+    def __init__(self, lang: str, props: ModelProperties, proc: ProcessManager):
+        self._lang = lang
+        self._props = props
+        self._proc = proc
+
+    def extract_recognition_model(self):
+        process_params = [
+            'combine_tessdata', '-e',
+            f'{self._props.tesseract_env}/{self._lang}.traineddata',
+            f'{self._lang}.lstm'
+        ]
+        process = self._proc.create_process(process_params)
+        return process
