c8fc8fe17cebe9a6a4aad2ee950ffaa6c926cf75
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-27 17:23:24 +0200

    update

--- app/services/tesseract/scripts/finetune.sh
+++ app/services/tesseract/scripts/finetune.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-lang="lav"
+lang="eng"
 
 lstmtraining \
-	--continue_from $lang.lstm \
-	--model_output model_output/hypermarket \
+	--continue_from ./$lang.lstm \
+	--model_output ./model/ \
 	--traineddata $TESSDATA_PREFIX/$lang.traineddata \
-	--train_listfile train_data/$lang.training_files.txt \
+	--train_listfile ./training_data/$lang.training_files.txt \
 	--max_iterations 400
 	#&> train.log
