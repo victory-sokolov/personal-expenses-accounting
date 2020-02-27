f7ae5883b4b4b94c14ad2edc2972b82d7bbc2b69
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-27 08:44:46 +0200

    modified run_task func

--- app/services/tesseract/Pipeline.py
+++ app/services/tesseract/Pipeline.py
@@ -2,6 +2,8 @@ from typing import List
 
 from app.services.tesseract.ModelProperties import ModelProperties
 from app.services.tesseract.PipelineBuilder import PipelineBuilder
+from app.services.tesseract.ProcessManager import ProcessManager
+from app.services.tesseract.TrainingDataGenerator import TrainingDataGenerator
 from app.utils.ClassMetrics import ClassMetrics
 
 
@@ -12,13 +14,14 @@ class Pipeline(object):
 
     def methods(self, class_name) -> List:
         methods = ClassMetrics.get_list_of_methods(class_name)
-        return [getattr(class_name, method) for method in methods]
+        return methods
 
     def run_tasks(self) -> None:
         for task in self.pipeline_tasks:
             methods = self.methods(task)
-            for execute in methods:
-                execute()
+            for method in methods:
+                func = getattr(task, method)
+                func()
 
 
 model = ModelProperties('eng')
