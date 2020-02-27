062235231218273715befb7017288618380817be
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-27 08:43:17 +0200

    fixed name

--- app/services/tesseract/ModelProperties.py
+++ app/services/tesseract/ModelProperties.py
@@ -1,11 +1,12 @@
 import os
 
+
 class ModelProperties(object):
 
-    model_path = os.getenv('MODEl_PATH')
+    model_path = os.getenv('MODEL_PATH')
     tesseract_env = os.getenv("TESSDATA_PREFIX")
     training_data = os.getenv("TRAINING_DATA")
-    iterations = 4500
+    iterations = 500
     pages = 5  # amount of training data (pages)
 
     def __init__(self, lang):
