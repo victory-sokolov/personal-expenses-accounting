6b91d89a7ca39363ea4890165f278469a78f21d0
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-29 16:20:35 +0200

    update

--- app/services/tesseract/ModelProperties.py
+++ app/services/tesseract/ModelProperties.py
@@ -6,21 +6,19 @@ class ModelProperties(object):
     model_path = os.getenv('MODEL_PATH')
     tesseract_env = os.getenv("TESSDATA_PREFIX")
     training_data = os.getenv("TRAINING_DATA")
+    stats = os.getenv("STATS_PATH")
     iterations = 500
     pages = 5  # amount of training data (pages)
 
-    def __init__(self, lang):
+    def __init__(self, lang, iterations=500, pages=5):
         self.lang = lang
+        self.iterations = iterations
+        self.pages = pages
 
     def init_setup(self):
         # empty folder and create new
-        dirs = [self.model_path, self.training_data]
+        dirs = [self.model_path, self.training_data, self.stats]
         for dirr in dirs:
             if os.path.exists(dirr):
                 shutil.rmtree(dirr)
             os.mkdir(dirr)
-
-        # delete stats csv if exists
-        stats_csv = './model_statistics.csv'
-        if os.path.isfile(stats_csv):
-            os.remove(stats_csv)
