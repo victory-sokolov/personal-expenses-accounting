772382fb841926b8da0caf31d4d2e50a28a6d58a
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-02 10:56:30 +0200

    refactored thresholding

--- app/Recogniser.py
+++ app/Recogniser.py
@@ -1,6 +1,7 @@
 import os
 
 import cv2
+import numpy as np
 import pytesseract
 from PIL import Image
 
@@ -14,23 +15,24 @@ class Recogniser(object):
     def binarize_image(self, image):
         img = cv2.imread(image)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.threshold(
+        thresh = cv2.threshold(
             gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-        blur_img = cv2.medianBlur(gray, 3)
-        cv2.imwrite('output.png', blur_img)
+
+        cv2.imwrite('output.png', thresh)
+
 
     def recognise_image(self, filename):
         tessdata_dir_config = r'--tessdata-dir ""'
-        config = (f'-l {self._lang} --oem 1 --psm 3')
+        config = (f'-l {self._lang}+"eng" --oem 1 --psm 3')
         text = pytesseract.image_to_string(
             Image.open(filename), config=config
         )
         return text
 
 
-img = "IMG_20200123_175929.jpg"
+image = "IMG_20200123_175929.jpg"
 recogniser = Recogniser("lav")
-recogniser.binarise_image(img)
+recogniser.binarize_image(image)
 
-text = recogniser.recognise_image('output.png')
-print(text)
+output_text = recogniser.recognise_image('output.png')
+print(output_text)
