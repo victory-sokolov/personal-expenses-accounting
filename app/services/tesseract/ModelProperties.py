ac9f76757dbf0d37c842a8567118fbf7993f1943
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-03 10:01:13 +0200

    init_setup modified

--- app/services/tesseract/ModelProperties.py
+++ app/services/tesseract/ModelProperties.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 
+
 class ModelProperties(object):
 
     model_path = os.getenv('MODEL_PATH')
     tesseract_env = os.getenv("TESSDATA_PREFIX")
     training_data = os.getenv("TRAINING_DATA")
     stats = os.getenv("STATS_PATH")
-
 
     def __init__(self, lang, iterations=4500, pages=250):
         self.lang = lang
@@ -18,6 +18,6 @@ class ModelProperties(object):
         # empty folder and create new
         dirs = [self.model_path, self.training_data, self.stats]
         for dirr in dirs:
-            if os.path.exists(dirr):
-                shutil.rmtree(dirr)
-            os.mkdir(dirr)
+            if not os.path.exists(dirr):
+                # shutil.rmtree(dirr)
+                os.mkdir(dirr)
