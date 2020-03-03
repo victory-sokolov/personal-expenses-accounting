8f6983df0d3607347ea78cfdd03227881083434f
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-03 10:07:34 +0200

    get date & get vendor name

--- app/Recogniser.py
+++ app/Recogniser.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import cv2
 import numpy as np
@@ -20,7 +21,6 @@ class Recogniser(object):
 
         cv2.imwrite('output.png', thresh)
 
-
     def recognise_image(self, filename):
         tessdata_dir_config = r'--tessdata-dir ""'
         config = (f'-l {self._lang}+"eng" --oem 1 --psm 3')
@@ -29,6 +29,26 @@ class Recogniser(object):
         )
         return text
 
+    def get_date(self):
+        output_text = recogniser.recognise_image('output.png').split("\n")
+
+        date_pattern = r'(0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](19|20)\d\d'
+        for line in output_text:
+            date = re.search(date_pattern, line)
+            if date:
+                print(date)
+                return date
+
+    def get_vendor(self):
+        pattern = ["SIA", "As"]
+        output_text = recogniser.recognise_image('output.png').split("\n")
+        for line in output_text:
+            vendor = re.search(r'\bSIA|As\b', line)
+            if vendor:
+                return vendor
+
+        def get_price(self):
+            pass
 
 image = "IMG_20200123_175929.jpg"
 recogniser = Recogniser("lav")
@@ -36,3 +56,6 @@ recogniser.binarize_image(image)
 
 output_text = recogniser.recognise_image('output.png')
 print(output_text)
+
+recogniser.get_vendor()
+recogniser.get_date()
