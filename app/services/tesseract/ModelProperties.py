5a8f696c82323785e26bcc0bc7e06edcf6eefd9b
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-04 17:58:36 +0200

    update

--- app/services/tesseract/ModelProperties.py
+++ app/services/tesseract/ModelProperties.py
@@ -9,13 +9,14 @@ class ModelProperties(object):
     training_data = os.getenv("TRAINING_DATA")
     stats = os.getenv("STATS_PATH")
 
-    def __init__(self, lang, iterations=4500, pages=250):
+    def __init__(self, lang, iterations=4500, pages=300):
         self.lang = lang
         self.iterations = iterations
         self.pages = pages
 
     def init_setup(self):
         # empty folder and create new
+        shutil.rmtree(self.stats)
         dirs = [self.model_path, self.training_data, self.stats]
         for dirr in dirs:
             if not os.path.exists(dirr):
