169022f541211e31d9c2192fa4ccc4fa865e6df4
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-05 10:06:51 +0200

    if model is extracted skip extraction step

--- app/services/tesseract/ModelExtractor.py
+++ app/services/tesseract/ModelExtractor.py
@@ -1,8 +1,8 @@
+import os
 import subprocess
 
 from app.services.tesseract.ModelProperties import ModelProperties
 from app.services.tesseract.OrderedClassMembers import OrderedClassMembers
-
 from app.services.tesseract.ProcessManager import ProcessManager
 
 
@@ -14,10 +14,14 @@ class ModelExtractor(metaclass=OrderedClassMembers):
         self._proc = proc
 
     def extract_recognition_model(self):
+        model_path = f'{self._props.default_model_path}/{self._lang}.lstm'
+        if os.path.exists(model_path):
+            return
+
         process_params = [
             'combine_tessdata', '-e',
             f'{self._props.tesseract_env}/{self._lang}.traineddata',
-            f'{self._lang}.lstm'
+            f'{model_path}'
         ]
         process = self._proc.create_process(process_params)
         return process.returncode
