9b9181670218c9ec8f7874b669c8b148c34e9131
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-10 21:30:45 +0200

    field for current fonts

--- app/services/tesseract/ModelProperties.py
+++ app/services/tesseract/ModelProperties.py
@@ -10,6 +10,7 @@ class ModelProperties(object):
     training_data = os.getenv("TRAINING_DATA")
     stats = os.getenv("STATS_PATH")
     trained_data = model_path
+    font = None
 
     def __init__(self, lang, iterations=4500, pages=300):
         self.lang = lang
