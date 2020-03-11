c8e9ab9cb41383d7f533f9d2060b124b8f67c485
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-11 19:06:31 +0200

    create navigation stack

--- app/services/mobile_app/App.js
+++ app/services/mobile_app/App.js
@@ -1,24 +1,20 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import {StyleSheet, View} from 'react-native';
+import CameraActivity from './components/CameraActivity';
+import HomeScreen from './components/HomeScreen';
 
-const App = () => {
-  return (
-    <View>
-      <RNCamera style={styles.cameraStyles} />
-      <Text>Take Photo</Text>
-    </View>
-  );
-};
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import Home from './components/HomeScreen';
+import CameraActivity from './components/CameraActivity';
 
-const styles = StyleSheet.create({
-  cameraStyles: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
+const Navigator = createStackNavigator({
+  Home: {screen: Home},
+  Camera: {screen: CameraActivity},
 });
+
+const App = createAppContainer(Navigator);
 
 export default App;
