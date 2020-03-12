0056f788731c796ba6312032573450ded079331e
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-12 10:40:18 +0200

    push button to bottom

--- app/services/mobile_app/components/HomeScreen.js
+++ app/services/mobile_app/components/HomeScreen.js
@@ -1,38 +1,50 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import {View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/EvilIcons';
 
+import CameraIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+
 class HomeScreen extends React.Component {
+
   render() {
-    const {navigate} = this.props.navigation;
+
+    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.cameraButton}
-          onPress={() => navigate('Camera')}>
-          <Icon name="camera" size={45} color="#fff" />
-        </TouchableOpacity>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View style={styles.footer}>
+          <TouchableOpacity
+              style={styles.cameraButton} 
+              onPress = {() => navigate(
+              'Camera'
+            )} >
+            <Icon name="camera" size={32} color='#fff'/>
+          </TouchableOpacity>       
+        </View>
+
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
   cameraButton: {
-    borderWidth: 1,
-    borderColor: 'rgba(0,0,0,0.2)',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 70,
-    height: 70,
-    backgroundColor: '#1E90FF',
-    borderRadius: 50,
+    borderWidth:1,
+    borderColor:'rgba(0,0,0,0.2)',
+    alignItems:'center',
+    justifyContent:'center',
+    width:50,
+    height:50,
+    backgroundColor:'#1E90FF',
+    borderRadius:50,
   },
+  footer: {
+    flex: 1,
+    justifyContent:'flex-end',
+    paddingBottom: 25,
+  }
 });
 
 export default HomeScreen;
