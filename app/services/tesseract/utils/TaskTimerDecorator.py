8db2bed7dd3c1b3e32e1632992a1ff4f949fac8b
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-13 14:36:05 +0200

    moved logger into method

--- app/services/tesseract/utils/TaskTimerDecorator.py
+++ app/services/tesseract/utils/TaskTimerDecorator.py
@@ -1,9 +1,6 @@
 import logging
 from datetime import datetime
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
 
 class TaskTimerDecorator:
 
@@ -14,4 +11,7 @@ class TaskTimerDecorator:
         start = datetime.now()
         self.task.run_tasks()
         elapsed_time = datetime.now() - start
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s'
+                            )
         logging.info('Elapsed time: %s', elapsed_time)
