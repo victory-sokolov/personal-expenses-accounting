d1f62a4ce2bbd4a31e3e54deaa35254050662205
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-15 11:01:59 +0200

    changed directory where to save pictures

--- app/services/mobile_app/utils/dirStorage.js
+++ app/services/mobile_app/utils/dirStorage.js
@@ -2,8 +2,8 @@ import { Platform } from 'react-native';
 const RNFS = require('react-native-fs');
 
 export const dirHome = Platform.select({
-  ios: RNFS.DocumentDirectoryPath,
-  android: RNFS.DocumentDirectoryPath,
+  ios: `${RNFS.DocumentDirectoryPath}/ReceiptApp`,
+  android: `${RNFS.ExternalDirectoryPath}/ReceiptApp`,
 });
 
 export const dirPictures = `${dirHome}/Pictures`;
