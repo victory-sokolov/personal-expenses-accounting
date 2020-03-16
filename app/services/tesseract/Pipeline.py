1502415fe1bd555593b118da739c77493eb6de3d
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-16 22:18:56 +0200

    pass --evaluate parameter when running Pipeline file

--- app/services/tesseract/Pipeline.py
+++ app/services/tesseract/Pipeline.py
@@ -1,13 +1,16 @@
 import inspect
-from typing import List
-from multiprocessing import Process, current_process
 import os
+from multiprocessing import Process, current_process
+from typing import List
+
+import click
+
 from app.services.tesseract.ModelProperties import ModelProperties
 from app.services.tesseract.PipelineBuilder import PipelineBuilder
 from app.services.tesseract.ProcessManager import ProcessManager
 from app.services.tesseract.TrainingDataGenerator import TrainingDataGenerator
-from app.utils.ClassMetrics import ClassMetrics
-from app.utils.TaskTimerDecorator import TaskTimerDecorator
+from app.services.tesseract.utils.ClassMetrics import ClassMetrics
+from app.services.tesseract.utils.TaskTimerDecorator import timing
 
 
 class Pipeline():
@@ -15,27 +18,29 @@ class Pipeline():
     def __init__(self, pipeline_tasks: List):
         self.pipeline_tasks = pipeline_tasks
 
-    def run_tasks(self) -> None:
-        for task in self.pipeline_tasks:
-            for current_task in task:
-                methods = current_task.__ordered__
-                for method in methods:
-                    func = getattr(current_task, method)
-                    if inspect.ismethod(func):
-                        func()
+    @timing
+    def run_tasks(self, evaluate) -> None:
+        tasks = self.pipeline_tasks[0]
+        if evaluate == "False":
+            tasks = [task for task in tasks if "Evaluator" not in str(task)]
+
+        for task in tasks:
+            methods = task.__ordered__
+            for method in methods:
+                func = getattr(task, method)
+                if inspect.ismethod(func):
+                    func()
 
 
-PIPELINE_LIST = [
-    PipelineBuilder(ModelProperties('lav', 50, 5)).create_pipeline(),
-    PipelineBuilder(ModelProperties('eng', 50, 5)).create_pipeline()
-]
+@click.command()
+@click.option('--evaluate', default=True, help='Evaluate model before and after training.')
+def main(evaluate):
+    PIPELINE_LIST = [
+        # PipelineBuilder(ModelProperties('lav')).create_pipeline(),
+        PipelineBuilder(ModelProperties('eng', 50, 10)).create_pipeline()
+    ]
+    Pipeline(PIPELINE_LIST).run_tasks(evaluate)
 
-TaskTimerDecorator(Pipeline(PIPELINE_LIST)).timer()
 
-# for proc in PIPELINE_LIST:
-
-#     process = Process(target=TaskTimerDecorator(
-#         Pipeline(PIPELINE_LIST)).timer()
-#     )
-#     process.start()
-#     process.join()
+if __name__ == "__main__":
+    main()
