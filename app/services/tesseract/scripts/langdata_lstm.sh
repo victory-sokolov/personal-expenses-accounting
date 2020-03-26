6b82c632644a43e4548f98691157a479b0a2ebf6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-26 20:04:49 +0200

    download language settings

---
+++ app/services/tesseract/scripts/langdata_lstm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+mkdir $TESSDATA_PREFIX/langdata_lstm
+
+cd $TESSDATA_PREFIX/langdata_lstm && \
+    wget https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/Latin.unicharset \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/Latin.xheights \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/common.punc \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/common.unicharambigs \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/desired_bigrams.txt \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/font_properties \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/forbidden_characters_default \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/radical-stroke.txt
+
+
+# eng data
+mkdir eng && cd eng
+wget https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/desired_characters \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/eng.numbers \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/eng.punc \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/eng.singles_text \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/eng.training_text \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/eng.unicharambigs \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/eng.unicharset \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/eng.wordlist \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/eng/okfonts.txt
+cd ..
+
+# lav data
+mkdir lav && cd lav
+wget https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/lav/desired_characters \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/lav/lav.numbers \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/lav/lav.punc \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/lav/lav.singles_text \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/lav/lav.training_text \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/lav/lav.unicharset \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/lav/lav.wordlist \
+    https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/lav/okfonts.txt
+cd ..
+
+# traineddata_best
+mkdir traineddata && cd traineddata
+wget https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/master/eng.traineddata \
+    https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/master/lav.traineddata
