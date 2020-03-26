0512cb600487eead310d5e7b833a9e26c86d6248
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-26 22:32:27 +0200

    refactored

--- app/services/tesseract/utils/font.py
+++ app/services/tesseract/utils/font.py
@@ -5,7 +5,7 @@ from pathlib import PurePosixPath
 from subprocess import PIPE, Popen, check_output
 from typing import List
 
-from app.services.tesseract.utils.helpers import read_json
+from utils.helpers import read_json
 
 
 def font_path() -> str:
@@ -20,7 +20,7 @@ def get_fonts_from_dir() -> List:
     Get font names in directory
     with extension: .ttf
     """
-    directory = f'{font_path()}'
+    directory = './fonts/'
     dir_content = os.listdir(directory)
     font_list = [font for font in dir_content if font.endswith('.ttf', -4)]
     return font_list
@@ -28,13 +28,13 @@ def get_fonts_from_dir() -> List:
 
 def get_font_names() -> List:
     """Get original font names."""
-    path = font_path()
+    path = './fonts/'
     fonts_output = check_output([
         'text2image',
         '--fonts_dir', path,
         '--list_available_fonts'
     ], text=True)
-    fonts=re.sub('[0-9]*:', '', fonts_output).split("\n")
+    fonts = re.sub('[0-9]*:', '', fonts_output).split("\n")
     return [font.strip() for font in fonts if font]
 
 
@@ -79,8 +79,8 @@ def is_lang_supported(font: str, lang: str) -> bool:
     # by default eng is supported mostly for all fonts
     if lang == 'eng' or lang == 'en':
         return True
-
-    full_path = os.path.join(font_path(), f'{font}')
+    path = './fonts/'
+    full_path = os.path.join(path, f'{font}')
     with Popen(['fc-query', full_path], stdout=PIPE, stderr=PIPE) as proc:
         langs = check_output([
             'grep', '-w', 'lang'
@@ -101,11 +101,11 @@ def supported_fonts(fonts, lang: str) -> List:
             skip = fonts[font]['skip']
             # if skip is false
             font = f'{font}.ttf'
+            #print(font)
             if not skip and is_lang_supported(font, lang):
                 fonts_list.append(font.split(".")[0])
     else:
         for font in fonts:
             if is_lang_supported(font, lang):
                 fonts_list.append(font.split(".")[0])
-
     return fonts_list
