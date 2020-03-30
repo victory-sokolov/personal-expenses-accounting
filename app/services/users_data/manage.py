5c37fbc119aea2f5557b9516c78ac60b0b278875
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-30 22:35:18 +0300

    moved routing to manage file

--- app/services/users_data/manage.py
+++ app/services/users_data/manage.py
@@ -1,8 +1,10 @@
-import create_app
+import os
 import unittest
-import coverage
 
+import coverage
 from flask.cli import FlaskGroup
+from src import create_app
+from src.controller.AddReceipt import AddReceipt
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -10,13 +12,15 @@ cli = FlaskGroup(create_app=create_app)
 @cli.command()
 def test():
     """Run tests without code coverage"""
-    tests = unittests.TestLoader().discover('/tests', pattern='test*.py')
+    tests = unittest.TestLoader().discover('/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
     return 1
 
 
+# Routings
+app.add_url_rule('/addreceipt', view_func=AddReceipt.as_view('addreceipt'))
 
 if __name__ == "main":
     cli()
