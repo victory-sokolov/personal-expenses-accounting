b68fa60ffb3da5f291cec8b072cd46786d9c42e5
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-30 22:37:05 +0300

    add CORS and UPLOAD_FOLDER path

--- app/services/users_data/src/__init__.py
+++ app/services/users_data/src/__init__.py
@@ -1,7 +1,22 @@
+import os
+from pathlib import Path
+
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
+from src import config
 
 
 def create_app():
 
     app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)
+    os.chdir('..')
+    UPLOAD_FOLDER = os.path.abspath(os.curdir) + '/recogniser'
+
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
     return app
