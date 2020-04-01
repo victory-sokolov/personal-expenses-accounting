3222ce8ab701e8cefc0264f58e7ef7bb8db5b02d
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-01 22:41:15 +0300

    increase size of photo icon

--- app/services/mobile_app/components/HomeScreen.js
+++ app/services/mobile_app/components/HomeScreen.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/EvilIcons';
 
-
 class HomeScreen extends React.Component {
-
   render() {
-    const { navigate } = this.props.navigation;
+    const {navigate} = this.props.navigation;
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <View style={styles.footer}>
@@ -22,24 +20,23 @@ class HomeScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-
   cameraButton: {
     shadowColor: 'black',
     shadowOpacity: 0.9,
     elevation: 12,
-    borderColor:'rgba(0,0,0,0.2)',
-    alignItems:'center',
-    justifyContent:'center',
-    width:60,
-    height:60,
-    backgroundColor:'#1E90FF',
-    borderRadius:50,
+    borderColor: 'rgba(0,0,0,0.2)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 70,
+    height: 70,
+    backgroundColor: '#1E90FF',
+    borderRadius: 50,
   },
   footer: {
     flex: 1,
-    justifyContent:'flex-end',
+    justifyContent: 'flex-end',
     paddingBottom: 25,
-  }
+  },
 });
 
 export default HomeScreen;
