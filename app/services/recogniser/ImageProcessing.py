cc2f53537872574654a6dc17477729f1038039ab
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-04 12:07:07 +0300

    rotate method

--- app/services/recogniser/ImageProcessing.py
+++ app/services/recogniser/ImageProcessing.py
@@ -1,7 +1,10 @@
 from functools import reduce
+from time import sleep
 
 import cv2
 import numpy as np
+from wand.image import Image as WandImage
+
 
 from base.ProcessManager import ProcessManager
 
@@ -9,6 +12,7 @@ from base.ProcessManager import ProcessManager
 class ImageProcessing:
 
     def __init__(self, image):
+        self.image_name = image
         self.image = cv2.imread(image)
 
     def change_image_DPI(self, image):
@@ -17,15 +21,28 @@ class ImageProcessing:
         ]
         process = ProcessManager.create_process(process_params)
 
+    def rotate(self, image):
+        angle = 90
+        (h, w) = image.shape[:2]
+        if h < w:
+            with WandImage(filename=self.image_name) as img:
+                img.rotate(angle)
+                img_buffer = np.asarray(
+                    bytearray(img.make_blob()), dtype=np.uint8)
+
+            if img_buffer is not None:
+                retval = cv2.imdecode(img_buffer, cv2.IMREAD_UNCHANGED)
+                return retval
+
     def gray_scale(self, image):
         return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     def noise_removal(self, image):
         return cv2.GaussianBlur(image, (5, 5), 0)
 
-    def binarize_image(self, image):
+    def thresh(self, image):
         return cv2.threshold(
-            image, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU
+            image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU
         )[1]
 
     def save_image(self, image):
@@ -46,11 +63,14 @@ class ImageProcessing:
         return rotated
 
     def run_pipeline(self):
+        # self.rotate(self.image)
+
         return reduce(
             lambda image, function: function(image), (
+                self.rotate,
                 self.gray_scale,
                 self.noise_removal,
-                self.binarize_image,
+                self.thresh,
                 self.save_image
             ),
             self.image
