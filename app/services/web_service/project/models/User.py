7c753b6bf974ad302e8c7aa4b5d1973ff2f0c900
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-05 19:41:33 +0300

    add new user to db

--- app/services/web_service/project/models/User.py
+++ app/services/web_service/project/models/User.py
@@ -4,21 +4,14 @@ from project import db
 
 
 class User(db.Model):
-    __tablename__ = "User"
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255))
     email = db.Column(db.String(70))
-
-    def create_user(self, user):
-        name = user['name']
-        email = user['email']
-        create_user = User(name, email)
-        db.session.add(create_user)
-        db.session.commit()
-
-        if create_user:
-            return jsonify({'status': 'New user has benn added!'}, 200)
-        return jsonify({'status': 'Failed to add user'}, 400)
-
-    def update_user_by_id(self, id):
-        pass
+    password = db.Column(db.String(255))
