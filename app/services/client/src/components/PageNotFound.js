4d7005f73aa7d21abb677e613414205411819012
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-06 23:01:31 +0300

    PageNotFound component

---
+++ app/services/client/src/components/PageNotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PageNotFound extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>404 Page Not Found</h1>
+      </div>
+    )
+  }
+}
+
+export default PageNotFound;
