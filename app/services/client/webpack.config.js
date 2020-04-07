b14fa1b60c0b10777b79062f7ac4f29e776d0f2f
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-07 21:33:47 +0300

    configs update

--- app/services/client/webpack.config.js
+++ app/services/client/webpack.config.js
@@ -1,57 +1,66 @@
-const path = require('path');
-const htmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const htmlWebPackPlugin = require("html-webpack-plugin");
 
 const settings = {
-  distPath: path.join(__dirname, "/dist"),
-  srcPath: path.join(__dirname, "/")
+	distPath: path.join(__dirname, "/dist"),
+	srcPath: path.join(__dirname, "/"),
 };
 
 function srcPathExtend(subpath) {
-  return path.join(settings.srcPath, subpath)
+	return path.join(settings.srcPath, subpath);
 }
 
 module.exports = {
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/react', {
-                'plugins': ['@babel/plugin-proposal-class-properties']
-              }]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              // limit: 8192,
-            },
-          },
-        ],
-      }
-    ]
-  },
-  plugins: [
-    // new htmlWebPackPlugin({
-    //   template: srcPathExtend("index.html")
-    // })
-  ],
-  watch: true
-}
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							[
+								"@babel/preset-env",
+								{
+									targets: {
+										esmodules: true,
+									},
+								},
+							],
+							"@babel/react",
+							{
+								plugins: ["@babel/plugin-proposal-class-properties"],
+							},
+						],
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				loader: "style-loader!css-loader",
+			},
+			{
+				test: /\.(png|jpg|gif)$/i,
+				use: [
+					{
+						loader: "url-loader",
+						options: {
+							// limit: 8192,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		// new htmlWebPackPlugin({
+		//   template: srcPathExtend("index.html")
+		// })
+	],
+	watch: true,
+};
