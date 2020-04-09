4cd39e4341ac79eb982479160dba93670589ddbd
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-09 21:50:34 +0300

    check if user with passed email exists

---
+++ app/services/web_service/project/controller/CreateUser.py
@@ -0,0 +1,39 @@
+import json
+
+from flask import Flask, current_app, jsonify, render_template, request
+from flask.views import MethodView
+
+from project import db
+from project.models.User import User
+
+
+class CreateUser(MethodView):
+
+    def post(self):
+        """Create new account"""
+        users_data = json.loads(request.data)
+        # add new user
+        name = users_data['name']
+        email = users_data['email']
+        password = users_data['password']
+        repeat_password = users_data['repeatPassword']
+
+        # check if user with that email exists
+        user_exists = User.query.filter_by(email=email)
+        if user_exists is None:
+            return jsonify({'status': 'User with that email already exists'}, 400)
+
+        if password != repeat_password:
+            return jsonify({"status": "Password doesn't match"})
+
+        if len(password) < 6:
+            return jsonify({'status': 'Password length must be 6 characters long'}, 400)
+
+        user = User(name=name, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'status': 'New user has been added!'}, 200)
+
+    def get(self):
+        return render_template('index.html')
