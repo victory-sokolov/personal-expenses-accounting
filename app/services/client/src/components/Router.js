51d7c6cb6ad11da936021cacc73f477202a0f68c
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-09 21:33:07 +0300

    added register route

--- app/services/client/src/components/Router.js
+++ app/services/client/src/components/Router.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
-
+import LoginFormContainer from "./Login/LoginFormContainer";
+import PageNotFound from './PageNotFound';
+import RegistrationFormContainer from "./Registration/RegistrationFormContainer";
 
 const Router = () => (
 	<BrowserRouter>
 		<Switch>
 			<Route exact path="/" component={App} />
-			{/* <Route path="/login" component={LoginFormContainer} />
+			<Route path="/login" component={LoginFormContainer} />
 			<Route path="/register" component={RegistrationFormContainer} />
-			<Route path="/dashboard" component={DashboardContainer} />
-			<Route component={PageNotFound} /> */}
+			{/* <Route path="/dashboard" component={DashboardContainer} /> */}
+			<Route component={PageNotFound} />
 		</Switch>
 	</BrowserRouter>
 );
