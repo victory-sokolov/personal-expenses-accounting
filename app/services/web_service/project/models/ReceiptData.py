c449c346682e2d2a35b1e684c628f4de9b8eafaa
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-09 21:42:05 +0300

    check hashed password metohd and removed __init__

--- app/services/web_service/project/models/ReceiptData.py
+++ app/services/web_service/project/models/ReceiptData.py
@@ -5,15 +5,13 @@ from project import db
 
 class ReceiptData(db.Model):
 
-    def __init__(self, vendor, price, date, category):
-        self.vendor = vendor
-        self.price = price
-        self.date = date
-        self.category = category
-
     __tablename__ = "receipt_data"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     vendor = db.Column(db.String(255))
     price = db.Column(db.String(20))
     date = db.Column(db.DateTime)
     category = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<ReceiptData vendor: {} \n price: {} \n date: {} category: {}>' \
+            .format(self.vendor, self.price, self.date, self.category)
--- app/services/web_service/project/models/User.py
+++ app/services/web_service/project/models/User.py
@@ -1,17 +1,23 @@
 from flask import jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from project import db
 
 
 class User(db.Model):
 
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
-
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255))
     email = db.Column(db.String(70))
     password = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<User name: {} \n email: {} \n password:{}>' \
+            .format(self.name, self.email, self.password)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
+
+    def get_password(self, password):
+        return check_password_hash(self.password, password)
