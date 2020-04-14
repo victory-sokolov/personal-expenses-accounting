8d7d82ad8089fe610745463bed7eae7bd6aaaf61
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-14 22:07:35 +0300

    select component

---
+++ app/services/client/src/components/Dashboard/ui-components/Select.js
@@ -0,0 +1,54 @@
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+
+
+const useStyles = makeStyles(theme => ({
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 265
+	},
+	selectEmpty: {
+		marginTop: theme.spacing(2)
+	}
+}));
+
+export default function NativeSelects() {
+	const classes = useStyles();
+	const [state, setState] = React.useState({
+		age: "",
+		name: "hai"
+	});
+
+	const handleChange = event => {
+		const name = event.target.name;
+		setState({
+			...state,
+			[name]: event.target.value
+		});
+	};
+
+	return (
+		<div>
+			<FormControl className={classes.formControl}>
+				<InputLabel htmlFor="age-native-simple">Age</InputLabel>
+				<Select
+					native
+					value={state.age}
+					onChange={handleChange}
+					inputProps={{
+						name: "age",
+						id: "age-native-simple"
+					}}
+				>
+					<option aria-label="None" value="" />
+					<option value={10}>Ten</option>
+					<option value={20}>Twenty</option>
+					<option value={30}>Thirty</option>
+				</Select>
+			</FormControl>
+		</div>
+	);
+}
