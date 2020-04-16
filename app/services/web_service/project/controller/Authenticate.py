6cadae5347dada8536cdb72dad889e5914f67370
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-16 14:28:33 +0300

    fixed password validation

--- app/services/web_service/project/controller/Authenticate.py
+++ app/services/web_service/project/controller/Authenticate.py
@@ -2,6 +2,7 @@ import json
 
 from flask import Flask, current_app, jsonify, render_template, request
 from flask.views import MethodView
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from project import db
 from project.models.User import User
@@ -16,7 +17,9 @@ class Authenticate(MethodView):
         password = users_credentials['password']
 
         user = User.query.filter_by(email=email).first()
-        if user and user.get_password(password):
+        validate_password = check_password_hash(user.password, password)
+
+        if user and validate_password:
             return jsonify({'status': 'User exists'}, 200)
 
         return jsonify({"status": "User not found"}, 400)
