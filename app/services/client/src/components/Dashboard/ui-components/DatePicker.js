abbed1c204c88a48c79ba15221ae54e76e1bfbb8
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-21 21:30:42 +0300

    set width of date component

--- app/services/client/src/components/Dashboard/ui-components/DatePicker.js
+++ app/services/client/src/components/Dashboard/ui-components/DatePicker.js
@@ -1,16 +1,25 @@
 import Utils from "@date-io/date-fns";
 import { createMuiTheme } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { ThemeProvider } from "@material-ui/styles";
 import React, { useState } from "react";
+import dashboard from "../dashboard.scss";
 
 const materialTheme = createMuiTheme({
 	spacing: 25,
 	minWidth: 265
 });
 
-export default function DatePickerComponent() {
+const useStyles = makeStyles((theme) => ({
+	root: {
+		"& > *": {
+			width: 305,
+		},
+	},
+}));
 
+export default function DatePickerComponent() {
 	const [selectedDate, handleDateChange] = useState(new Date());
 
 	return (
@@ -19,9 +28,9 @@ export default function DatePickerComponent() {
 				<KeyboardDatePicker
 					clearable
 					value={selectedDate}
-					onChange={date => handleDateChange(date)}
-					minDate={new Date()}
+					onChange={(date) => handleDateChange(date)}
 					format="MM/dd/yyyy"
+					className={dashboard.MuiInput}
 				/>
 			</ThemeProvider>
 		</MuiPickersUtilsProvider>
