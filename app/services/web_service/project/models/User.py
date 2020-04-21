bf1d48656562dae60818476bafdc152df01949f1
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-21 21:28:20 +0300

    confirm registration

--- app/services/web_service/project/models/User.py
+++ app/services/web_service/project/models/User.py
@@ -1,6 +1,6 @@
-from flask import jsonify
+from flask import jsonify, current_app
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from project import db
 
 
@@ -11,6 +11,7 @@ class User(db.Model):
     name = db.Column(db.String(255))
     email = db.Column(db.String(70))
     password = db.Column(db.String(255))
+    confirmed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '<User name: {} \n email: {} \n password:{}>' \
@@ -21,3 +22,21 @@ class User(db.Model):
 
     def get_password(self, password):
         return check_password_hash(self.password, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.user_id}).decode("utf-8")
+
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+
+        if data.get('confirm') != self.user_id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
