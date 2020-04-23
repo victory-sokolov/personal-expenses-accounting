fab748ece94b965d2243c20424d6a99fd6ddeddf
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-23 22:45:35 +0300

    send auth token

--- app/services/web_service/project/controller/CreateUser.py
+++ app/services/web_service/project/controller/CreateUser.py
@@ -1,9 +1,11 @@
 import json
 
-from flask import Flask, current_app, jsonify, render_template, request
+from flask import (Flask, current_app, jsonify, make_response, render_template,
+                   request)
 from flask.views import MethodView
 
 from project import db
+from project.auth import encode_auth_token
 from project.models.User import User
 
 
@@ -11,31 +13,32 @@ class CreateUser(MethodView):
 
     def post(self):
         """Create new account"""
-        users_data = json.loads(request.data)
-        print(users_data)
-        # add new user
-        name = users_data['name']
-        email = users_data['email']
-        password = users_data['password']
-        repeat_password = users_data['repeatPassword']
+        user_data = json.loads(request.data)
 
-        # check if user with that email exists
-        user_exists = User.query.filter_by(email=email).first()
+        user_exists = User.query.filter_by(email=user_data['email']).first()
         if user_exists:
-            print("User exists")
             return jsonify({'status': 'User with that email already exists'}, 400)
 
-        if password != repeat_password:
+        if user_data['password'] != user_data['repeatPassword']:
             return jsonify({"status": "Password doesn't match"})
 
-        if len(password) < 6:
+        if len(user_data['password']) < 6:
             return jsonify({'status': 'Password length must be 6 characters long'}, 400)
 
-        user = User(name=name, email=email)
-        user.set_password(password)
+        user = User(
+            name=user_data['name'],
+            email=user_data['email']
+        )
+        user.set_password(user_data['password'])
         db.session.add(user)
         db.session.commit()
-        return jsonify({'status': 'New user has been added!'}, 200)
+        auth_token = encode_auth_token(user.id)
+        response = {
+            'status': 'success',
+            'message': 'Successfully registered.',
+            'auth_token': auth_token.decode()
+        }
+        return jsonify(response), 200
 
     def get(self):
         return render_template('index.html')
