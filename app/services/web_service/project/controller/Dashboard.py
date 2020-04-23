d7a571e06f500ca994839b46f48698db900820fc
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-23 22:45:00 +0300

    login_required to access dashboard route

--- app/services/web_service/project/controller/Dashboard.py
+++ app/services/web_service/project/controller/Dashboard.py
@@ -1,8 +1,9 @@
 from flask import Flask, current_app, jsonify, render_template, request
 from flask.views import MethodView
 
+from flask_login import login_required, current_user
 
 class Dashboard(MethodView):
-
+    @login_required
     def get(self):
         return render_template("index.html")
