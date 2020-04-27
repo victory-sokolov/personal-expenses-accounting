fec8e644bc2edc2599204999ece193227f610e2d
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-27 22:39:43 +0300

    UserAPI

---
+++ app/services/web_service/project/controller/UserAPI.py
@@ -0,0 +1,20 @@
+from flask import jsonify
+from flask.views import MethodView
+
+from project.models.User import User
+
+
+class UserAPI(MethodView):
+
+    def get(self, id):
+        user = User.query.filter_by(id=id).first()
+        if user:
+            user_object = {
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+                'receipts': user.receipts
+            }
+            return jsonify(user_object), 200
+
+        return jsonify({'status': 'User not found!'}), 401
