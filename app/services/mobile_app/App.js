0eeb2868c344842f4692be419850234e3737104d
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-04-28 20:54:12 +0300

    Login Screen

--- app/services/mobile_app/App.js
+++ app/services/mobile_app/App.js
@@ -1,12 +1,15 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import CameraActivity from './components/CameraActivity';
 import Home from './components/HomeScreen';
+import Login from './components/Login';
 
-import {createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
 
 const Navigator = createStackNavigator({
+  Login: {screen: Login},
   Home: {screen: Home},
-  Camera: {screen: CameraActivity},
+  Camera: {screen: CameraActivity}
+
 });
 
 const App = createAppContainer(Navigator);
