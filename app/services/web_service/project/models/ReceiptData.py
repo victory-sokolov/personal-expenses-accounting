b9ac421eaac0f1d7e5e3dce550a7ccc317d0c31d
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-01 18:56:15 +0300

    added __init__

--- app/services/web_service/project/models/ReceiptData.py
+++ app/services/web_service/project/models/ReceiptData.py
@@ -4,14 +4,18 @@ from project import db
 
 
 class ReceiptData(db.Model):
-
     __tablename__ = "receipt_data"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    image = db.Column(db.String(255), nullable=False)
     vendor = db.Column(db.String(255))
     price = db.Column(db.String(20))
     date = db.Column(db.DateTime)
     category = db.Column(db.String(50))
     warranty = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, **kwargs):
+        super(ReceiptData, self).__init__(**kwargs)
 
     def __repr__(self):
         return '<ReceiptData vendor: {} \n price: {} \n date: {} category: {}>' \
