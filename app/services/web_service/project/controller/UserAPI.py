01fe76a5433e00a3fd166cba2e34f8bdf3c3b0e6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-01 18:54:59 +0300

    convert sqlalchemy object to python dict

--- app/services/web_service/project/controller/UserAPI.py
+++ app/services/web_service/project/controller/UserAPI.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import jsonify
 from flask.views import MethodView
 
@@ -8,14 +10,21 @@ class UserAPI(MethodView):
 
     def get(self, id):
         user = User.query.filter_by(id=id).first()
+        receipts = []
+        for receipt in user.receipts:
+            dictret = dict(receipt.__dict__)
+            dictret.pop('_sa_instance_state', None)
+            receipts.append(dictret)
+
         if user:
             user_object = {
                 'id': user.id,
                 'name': user.name,
                 'email': user.email,
                 'avatar': user.gravatar(),
-                'receipts': user.receipts
+                'receipts': receipts
             }
+
             return jsonify(user_object), 200
 
         return jsonify({'status': 'User not found!'}), 401
