44c94b022047bc19ca3fb430b3c701ad5457052b
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-03 19:51:10 +0300

    display avatar

--- app/services/client/src/components/Dashboard/Nav/Nav.js
+++ app/services/client/src/components/Dashboard/Nav/Nav.js
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import nav from './side-nav.scss';
 
-
 class Nav extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
     render() {
+		let dataName = this.props.userData.name;
+		const name = dataName.charAt(0).toUpperCase() + dataName.slice(1);
         return (
-			<nav className={nav.menu} tabIndex="0">
-				<div className="smartphone-menu-trigger"></div>
-				<header className={nav.avatar}>
-					<img src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" />
-					<h2>John D.</h2>
-				</header>
-				<ul>
-					<li tabIndex="0" className={nav.receiptIcon}>
-						<a href="#">Expenses</a>
-					</li>
-					<li tabIndex="0" className={nav.exportIcon}>
-						<a href="#">Reports</a>
-					</li>
-					<li tabIndex="0" className={nav.cogIcon}>
-						<a href="#">Settings</a>
-					</li>
-					<li tabIndex="0" className={nav.logoutIcon}>
-						<a href="#">Log out</a>
-					</li>
-				</ul>
-			</nav>
-		);
+					<nav className={nav.menu} tabIndex="0">
+						<div className="smartphone-menu-trigger"></div>
+						<header className={nav.avatar}>
+							<img src={this.props.userData.avatar} />
+							<h3>{name}</h3>
+						</header>
+						<ul>
+							<li tabIndex="0" className={nav.receiptIcon}>
+								<Link to="#">Expenses</Link>
+							</li>
+							<li tabIndex="0" className={nav.exportIcon}>
+								<Link to="#">Reports</Link>
+							</li>
+							<li tabIndex="0" className={nav.cogIcon}>
+								<Link to="#">Settings</Link>
+							</li>
+							<li tabIndex="0" className={nav.logoutIcon}>
+								<Link to="/logout">Log out</Link>
+							</li>
+						</ul>
+					</nav>
+				);
     }
 }
 
