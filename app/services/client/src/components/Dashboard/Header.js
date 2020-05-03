cd0ec84f415fb0a3ea9948dfe0772504ad64b420
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-03 23:11:11 +0300

    remode unused props

--- app/services/client/src/components/Dashboard/Header.js
+++ app/services/client/src/components/Dashboard/Header.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
+import AddNewReceiptContainer from './AddNewReceipt/AddNewReceiptContainer';
 import dashboard from "./dashboard.scss";
-import ModalWindow from './ModalWindow';
-
 class Header extends Component {
+
+
     render() {
         return (
 					<div className={dashboard.heading}>
 						<h1>Expenses</h1>
 						<div className={dashboard.divider}></div>
 						<div className={dashboard.filterandexpens}>
-							<div className={dashboard.filterIcon}></div>
-							<a href="#">Show filters</a>
-							<ModalWindow />
+							<div className={dashboard.filterSection}>
+								<div className={dashboard.filterIcon}></div>
+								<a href="#">Show filters</a>
+							</div>
+							<AddNewReceiptContainer
+								showModal={this.props.showModal}
+								imagePreviewHandler={this.props.imagePreviewHandler}
+								imagePreview={this.props.imagePreview}
+							/>
 						</div>
-
 						<div className={dashboard.divider}></div>
 					</div>
 				);
-    }
+	}
 }
 
 export default Header;
