f862d204a80a9933ee1ec595d75f92ac60099087
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-03 23:08:08 +0300

    refactored

--- app/services/client/src/components/Dashboard/ModalWindow.js
+++ app/services/client/src/components/Dashboard/ModalWindow.js
@@ -10,33 +10,71 @@ import FileUpload from "./ui-components/FileUpload";
 import { InputContainer, InputOutline } from './ui-components/InputOutline';
 import NativeSelects from './ui-components/Select';
 
-
 class ModalWindow extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			show: false,
-			setShow: false,
+			image: null,
+			imagePath: null,
+			renderImageUpload: true,
+			vendor: "",
+			date: "",
+			amount: "",
+			category: "",
+			warranty: "",
 		};
 	}
 
-
-	handleClose = () => {
-		this.setState({ show: false});
-		this.props.componentMount();
+	setInputFields = () => {
+		this.setState({
+			vendor: this.props.inputData.vendor,
+			date: this.props.inputData.date,
+			amount: this.props.inputData.amount,
+			category: this.props.inputData.category,
+			warranty: this.props.inputData.warranty,
+		});
 	}
-	handleShow = () => this.setState({ show: true });
-	render() {
-		const { imagePath, renderImageUpload } = this.props.receiptData;
-		return (
-			<>
-				<div className={dashboard.buttonContainer} onClick={this.handleShow}>
-					<button>New Expense</button>
-				</div>
 
+	resetInputFields = () => {
+		this.setState({
+			vendor: "",
+			date: "",
+			amount: "",
+			category: "",
+			warranty: "",
+		});
+	}
+
+	onImagePreview = (e) => {
+		e.preventDefault();
+		let file = event.target.files[0];
+		this.setState({ image: file });
+		if (file != null) {
+			let image = URL.createObjectURL(file);
+			this.imagePreviewHandler(image);
+		}
+	};
+
+	// Discard image preview when modal is closed
+	imagePreviewHandler(image) {
+		this.setState((prevState) => ({
+			renderImageUpload: !prevState.renderImageUpload,
+			imagePath: image,
+		}));
+	}
+
+	onChangeHandler = () => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	render() {
+		return (
+			<div>
 				<Modal
-					show={this.state.show}
-					onHide={this.handleClose}
+					show={this.props.show}
+					onHide={this.props.handleClose}
 					size="lg"
 					centered
 				>
@@ -52,7 +90,8 @@ class ModalWindow extends Component {
 											label="Vendor"
 											type="text"
 											id="vendor"
-											onChange={this.props.onChangeHandler}
+											onChange={this.onChangeHandler}
+											value={this.state.vendor}
 										/>
 										<InputContainer>
 											<DatePickerComponent />
@@ -61,7 +100,8 @@ class ModalWindow extends Component {
 											label="Amount"
 											type="text"
 											id="amount"
-											onChange={this.props.onChangeHandler}
+											onChange={this.onChangeHandler}
+											value={this.amount}
 										/>
 										<InputContainer>
 											<NativeSelects />
@@ -70,14 +110,15 @@ class ModalWindow extends Component {
 											label="Warranty"
 											type="text"
 											id="warranty"
-											onChange={this.props.onChangeHandler}
+											onChange={this.onChangeHandler}
+											value={this.warranty}
 										/>
 									</Col>
 									<Col xs={12} md={6} className={dashboard.verticalCenterd}>
-										{renderImageUpload ? (
-											<FileUpload onChange={this.props.onImageHandler} />
+										{this.state.renderImageUpload ? (
+											<FileUpload onChange={this.onImagePreview} />
 										) : (
-											<img src={imagePath} alt="" />
+											<img src={this.state.imagePath} alt="Cash receipt" />
 										)}
 									</Col>
 								</Row>
@@ -85,15 +126,16 @@ class ModalWindow extends Component {
 						</Container>
 					</Modal.Body>
 					<Modal.Footer>
-						<Button variant="secondary" onClick={this.handleClose}>
+						<Button variant="secondary" onClick={this.props.handleClose}>
 							Close
 						</Button>
-						<Button variant="primary" onClick={this.handleClose}>
+						<Button variant="primary" onClick={this.props.handleClose}>
 							Save
 						</Button>
+						{this.props.children}
 					</Modal.Footer>
 				</Modal>
-			</>
+			</div>
 		);
 	}
 }
