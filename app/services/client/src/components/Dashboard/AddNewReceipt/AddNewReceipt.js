46d95565ff60fb7855d6db034cfbeeaac4a8a540
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-04 23:12:53 +0300

    AddNewReceiptContainer

---
+++ app/services/client/src/components/Dashboard/AddNewReceipt/AddNewReceipt.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import button from "../../ui-elements/button.scss";
+import dashboard from '../dashboard.scss';
+
+class AddNewReceipt extends Component {
+	constructor(props) {
+		super(props);
+    }
+
+
+	render() {
+		return (
+			<div className={dashboard.buttonContainer}>
+				<DropdownButton id="dropdown-basic-button" title="New Expense">
+					<Dropdown.Item onClick={this.props.showModal}>
+						Create Manually
+					</Dropdown.Item>
+					<input
+						type="file"
+						name="upload"
+						className={button.uploadButton}
+						ref={(input) => (this.props.inputFileRef.current = input)}
+						onChange={this.props.onImageHandler}
+					/>
+					<Dropdown.Item
+						onClick={this.props.uploadInputHandler}
+						title="Recognize Receipt"
+					>
+						Recognize Receipt
+					</Dropdown.Item>
+				</DropdownButton>
+			</div>
+		);
+	}
+}
+
+export default AddNewReceipt;
