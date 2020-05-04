b812c7a619f398f0f1f4cfb53bb06979fa1bb5d4
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-04 23:14:14 +0300

    LoginForm

--- app/services/client/src/components/Login/LoginForm.js
+++ app/services/client/src/components/Login/LoginForm.js
@@ -13,53 +13,53 @@ class LoginForm extends Component {
 		const {email, password} = this.props;
 
         return (
-					<form method="POST" onSubmit={this.props.handleSubmit}>
-						<div className={inputStyle.inputContainer}>
-							<input
-								type="text"
-								id="email"
-								className={inputStyle.input}
-								name="email"
-								required="required"
-								autoComplete="off"
-								value={email.value}
-								onChange={this.props.handleInputChange}
-							/>
-							<label htmlFor="email">Email</label>
-							<div className={inputStyle.bar}></div>
-						</div>
-						<div className={inputStyle.inputContainer}>
-							<input
-								type="password"
-								id="password"
-								className={inputStyle.input}
-								name="password"
-								required="required"
-								autoComplete="off"
-								value={password.value}
-								onChange={this.props.handleInputChange}
-							/>
-							<label htmlFor="password">Password</label>
-							<div className={inputStyle.bar}></div>
-						</div>
-						<div className={login.loginBlock}>
-							<div className={login.rememberMeBlock}>
-								<input type="checkbox" id="remember" name="remember" />
-								<label htmlFor="remember">Remember me</label>
-							</div>
-							<a href="#">Forgot password?</a>
-						</div>
-						<button className={buttonStyle.button}>Login</button>
-						<div className={login.loginFooter}>
-							<p>
-								Don’t have an account?
-								<Link to="/register" className="nav-link">
-									Click here
-								</Link>
-							</p>
-						</div>
-					</form>
-				);
+			<form method="POST" onSubmit={this.props.handleSubmit}>
+				<div className={inputStyle.inputContainer}>
+					<input
+						type="text"
+						id="email"
+						className={inputStyle.input}
+						name="email"
+						required="required"
+						autoComplete="off"
+						value={email.value}
+						onChange={this.props.handleInputChange}
+					/>
+					<label htmlFor="email">Email</label>
+					<div className={inputStyle.bar}></div>
+				</div>
+				<div className={inputStyle.inputContainer}>
+					<input
+						type="password"
+						id="password"
+						className={inputStyle.input}
+						name="password"
+						required="required"
+						autoComplete="off"
+						value={password.value}
+						onChange={this.props.handleInputChange}
+					/>
+					<label htmlFor="password">Password</label>
+					<div className={inputStyle.bar}></div>
+				</div>
+				<div className={login.loginBlock}>
+					<div className={login.rememberMeBlock}>
+						<input type="checkbox" id="remember" name="remember" />
+						<label htmlFor="remember">Remember me</label>
+					</div>
+					<a href="#">Forgot password?</a>
+				</div>
+				<button className={buttonStyle.button}>Login</button>
+				<div className={login.loginFooter}>
+					<p>
+						Don’t have an account?
+						<Link to="/register" className="nav-link">
+							Click here
+						</Link>
+					</p>
+				</div>
+			</form>
+		);
     }
 }
 
