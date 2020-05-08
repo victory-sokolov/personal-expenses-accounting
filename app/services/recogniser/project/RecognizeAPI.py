b20d2cb4ecd6c4cd718325112118ad3f3d9a23c6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-08 21:43:16 +0300

    renamed method save_image to imageToBAse64

--- app/services/recogniser/project/RecognizeAPI.py
+++ app/services/recogniser/project/RecognizeAPI.py
@@ -9,26 +9,26 @@ from flask.views import MethodView
 from PIL import Image
 from werkzeug.utils import secure_filename
 
-from project.Recognizer import recognise_factory
+from project.Recognizer import Recognizer
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 
 class RecognizeAPI(MethodView):
 
-    def allowed_file(self, filename):
+    def allowed_files(self, filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
     def file_upload(self):
         """Save image"""
         file = request.files['file']
         target = current_app.config.get('UPLOAD_FOLDER')
-        if file and self.allowed_file(file.filename):
+        if file and self.allowed_files(file.filename):
             filename = secure_filename(file.filename)
             destination = "/".join([target, filename])
             file.save(destination)
 
-    def save_image(self, image) -> str:
+    def imageToBase64(self, image) -> str:
         """Convert Base64 to image and return its name"""
         target = current_app.config.get('UPLOAD_FOLDER')
         date = datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
@@ -46,8 +46,8 @@ class RecognizeAPI(MethodView):
                 receipt_data = json.loads(request.data)
                 base64_image = receipt_data['image']
                 user_id = receipt_data['id']
-                image = self.save_image(base64_image)
-                recognise_factory(image, user_id)
+                image = self.imageToBase64(base64_image)
+                Recognizer('lav').recognise_factory(image, user_id)
                 return jsonify({'status': 'Image Recognized'}), 200
 
         return jsonify({'status': 'Cannot handle request!'}), 400
