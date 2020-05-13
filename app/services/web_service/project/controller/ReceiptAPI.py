321fe3c417738bdfe946333c98594ab52fc70de2
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-13 20:52:30 +0300

    get yearly spendings

--- app/services/web_service/project/controller/ReceiptAPI.py
+++ app/services/web_service/project/controller/ReceiptAPI.py
@@ -56,17 +56,27 @@ class ReceiptAPI(MethodView):
         db.session.commit()
         return jsonify({'status': f'Receipt with ID: {id} has been deleted'}), 200
 
+    def calculate_sum(self, data):
+        return sum([float(field.price)
+                    for field in data if field.price]
+                   )
+
     def get(self, id):
         today = datetime.datetime.today()
         year = today.year
         month = today.month
 
         monthly_data = ReceiptData.query.filter(ReceiptData.user_id == id,
-            ReceiptData.date.between(f'{year}-{month}-1', f'{year}-{month+1}-1')).all()
-        monthly_spendings = sum([float(field.price)
-                             for field in monthly_data if field.price])
+                                                ReceiptData.date.between(f'{year}-{month}-1', f'{year}-{month+1}-1')).all()
+        monthly_spendings = self.calculate_sum(monthly_data)
 
+        yearly_data = ReceiptData.query.filter(ReceiptData.user_id == id,
+                                               ReceiptData.date.between(f'{year}-1-1', f'{year}-12-31')).all()
+        yearly_spendings = self.calculate_sum(yearly_data)
+        print(monthly_data)
+        print(yearly_data)
         data = {
-            'monthly': monthly_spendings
+            'monthly': monthly_spendings,
+            'yearly': yearly_spendings
         }
         return jsonify(data), 200
