d95ba3ed2b3af7e82b4742c60e3d21cef1423a34
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-15 18:42:16 +0300

    pass data to chart

--- app/services/client/src/components/Dashboard/Charts/YearChart.js
+++ app/services/client/src/components/Dashboard/Charts/YearChart.js
@@ -7,16 +7,31 @@ class YearChart extends Component {
 		super(props);
 
 		this.state = {
+			receiptData: {},
+			months: [
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December",
+			],
 			series: [
 				{
-					name: "Expenses",
-					data: [23, 34, 12, 64, 32, 43, 23, 34, 12, 54, 32, 43]
-				}
+					name: "Expenses &euro;",
+					data: [],
+				},
 			],
 			options: {
 				chart: {
 					type: "line",
-					height: 250
+					height: 250,
 				},
 				markers: {
 					size: 6,
@@ -24,17 +39,17 @@ class YearChart extends Component {
 					strokeColors: "#fff",
 					strokeWidth: 2,
 					hover: {
-						size: 7
-					}
-                },
+						size: 7,
+					},
+				},
 				stroke: {
 					width: 4,
-					curve: "smooth"
+					curve: "smooth",
 				},
 				yaxis: {
 					labels: {
-						minWidth: 40
-					}
+						minWidth: 40,
+					},
 				},
 				xaxis: {
 					type: "text",
@@ -50,28 +65,68 @@ class YearChart extends Component {
 						"Sep",
 						"Oct",
 						"Nov",
-						"Dec"
-					]
+						"Dec",
+					],
 				},
 				title: {
 					text: "Spendings",
 					align: "left",
 					style: {
 						fontSize: "16px",
-						color: "#666"
-					}
-                },
-			}
+						color: "#666",
+					},
+				},
+			},
 		};
 	}
 
+	componentDidMount = () => {
+		const id = localStorage.getItem("id");
+		this.receiptAggregatedData(id);
+	};
+
+	receiptAggregatedData = async (id) => {
+		await fetch(`/receipt/${id}`, {
+			method: "GET",
+			cache: "no-cache",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState({ receiptData: data, isLoading: false }, () => this.yearlySpendings(data));
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	};
+
+	yearlySpendings = (receiptData) => {
+		let totalSumForMonth = [];
+		let receipts = receiptData.yearly;
+		this.state.months.map((value) => {
+			let sum = receipts[value].reduce((curr, prev) => curr + prev, 0);
+			totalSumForMonth.push(sum);
+		});
+		let monthData = {...this.state.series}[0];
+		monthData.data = [...totalSumForMonth];
+		this.setState({monthData});
+	};
+
 	render() {
+		const dat = [
+			{
+				name: "Expenses &euro;",
+				data: this.state.series[0].data,
+			}
+		];
 		return (
 			<div id="wrapper">
 				<div id="chart-area" className={cards.spendingsCharCart}>
 					<ReactApexChart
 						options={this.state.options}
-						series={this.state.series}
+						series={dat}
 						type="line"
 						height={250}
 					/>
