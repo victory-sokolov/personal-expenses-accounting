d9e212880e8885289ee93f224ac818920de72336
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-05-16 21:25:01 +0300

    return empty string if list is empty

--- app/services/recogniser/project/category/search_category.py
+++ app/services/recogniser/project/category/search_category.py
@@ -14,11 +14,11 @@ class SearchCategory(Parser1188):
         categories = []
         page = super().get_page()
         company = page.find_all("div", {"class": "company"})
-
         branches = [branch.find("div", {"class": "branch"})
                     for branch in company]
 
         for cat in branches:
             if cat:
                 categories.append(cat.find('a').text)
-        return categories[0]
+
+        return categories[0] if categories else ""
