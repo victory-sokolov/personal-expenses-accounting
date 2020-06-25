8cc1027753c26edff6eb8041292e095329d9601e
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-06-25 19:33:54 +0300

    base config update

--- app/services/web_service/project/tests/base.py
+++ app/services/web_service/project/tests/base.py
@@ -3,20 +3,14 @@ import unittest
 
 import pytest
 
+from config import config
 from project import create_app, db
 
-app = create_app()
+
 class BaseTestCase(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
-        self.app = create_app()
-        # self.app_context = self.app.app_context()
-        # self.app_context.push()
         super(BaseTestCase, self).__init__(*args, **kwargs)
-
-    def create_app(self):
-        app.config.from_object("project.config.DevelopmentConfig")
-        return app
 
     def setUp(self):
         db.create_all()
