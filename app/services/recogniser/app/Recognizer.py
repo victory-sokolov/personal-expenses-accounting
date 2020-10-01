6baa72733684a5580b04e4deffd0e10420ef0ba5
victory-sokolov <viktorsokolov.and@gmail.com>
2020-10-01 12:15:52 +0300

    fix imports

--- app/services/recogniser/app/Recognizer.py
+++ app/services/recogniser/app/Recognizer.py
@@ -1,19 +1,19 @@
 import json
 import os
 import re
+from datetime import datetime
 from typing import List
-
-import requests
-from PIL import Image
 
 import cv2
 import numpy as np
 import pytesseract
-from datetime import datetime
-from project.category.search_category import SearchCategory
-from project.ImageProcessing import ImageProcessing
+import requests
+from PIL import Image
 from redis import Redis
 from rq import Queue
+
+from app.category.search_category import SearchCategory
+from app.ImageProcessing import ImageProcessing
 
 
 class Recognizer(object):
@@ -21,8 +21,18 @@ class Recognizer(object):
     def __init__(self, lang: str):
         self._lang = lang
 
+    def easyocr_recognize(self, filename: str) -> List:
+        import easyocr
+
+        reader = easyocr.Reader(['lv'], gpu=False)
+        result = reader.readtext(filename, detail=0)
+
+        return list(
+            filter(None, result.split("\n"))
+        )
+
     def recognise_image(self, filename) -> List:
-        tessdata_dir = r'--tessdata-dir ' + f"{os.getcwd()}/project/data/"
+        tessdata_dir = r'--tessdata-dir ' + f"{os.getcwd()}/app/data/"
 
         traineddata = f'{self._lang}+eng+lav-9bc56f04-e9fa-4046-8118-788a91cb4e92'
         black_list_chars = '#~_|!?+»=<>[]();,:*”'
@@ -34,7 +44,7 @@ class Recognizer(object):
         text = pytesseract.image_to_string(
             Image.open(filename), config=config
         )
-        os.remove(f'{os.getcwd()}/project/output.png')
+        os.remove(f'{os.getcwd()}/app/output.png')
         return list(
             filter(None, text.split("\n"))
         )
@@ -63,12 +73,15 @@ class Recognizer(object):
         return ""
 
     def receipt_data(self, image, user_id):
-        path = f"{os.getcwd()}/project/output.png"
-        data = self.recognise_image(path)
+        path = f"{os.getcwd()}/app/output.png"
+        # data = self.recognise_image(path)
+        data = self.easyocr_recognize(path)
+
         vendor = self.get_vendor(data)
         date = self.get_date(data)
         price = self.get_price(data)
         category = SearchCategory(vendor).get_branch_by_shop_name()
+
         receipt_data = {
             'image': image.split("/")[-1],
             'vendor': vendor,
@@ -91,6 +104,8 @@ class Recognizer(object):
         q = Queue(connection=Redis())
 
         pipeline_job = q.enqueue(image_process.run_pipeline)
-        q.enqueue(recognize.receipt_data, args=(image, user_id), depends_on=pipeline_job)
+        q.enqueue(recognize.receipt_data, args=(
+            image, user_id), depends_on=pipeline_job)
 
-        print(f"Task {pipeline_job.id} added to queue at {pipeline_job.enqueued_at}. {len(q)} tasks in the queue")
+        print(
+            f"Task {pipeline_job.id} added to queue at {pipeline_job.enqueued_at}. {len(q)} tasks in the queue")
